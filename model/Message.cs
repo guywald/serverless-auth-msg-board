35d10ddc63c36826101d00be5681183d9289eda1
Guy Wald <guy@guywald.com>
2017-03-17 23:05:59 +0200

    change namespace, remove attribute

--- model/Message.cs
+++ model/Message.cs
@@ -1,12 +1,8 @@
-using Amazon.DynamoDBv2.DataModel;
-
 namespace ServerlessAuthMessageBoard.model
 {
     public class Message
     {
-        [DynamoDBHashKey]  
         public string Id { get; set; }
         public string MessageContent { get; set; }
-
     }
 }
